d2fbdd55d68ed6f59e280899c49470f0c7900e12
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-07-14 23:38:08 -0600

    Building DayList

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -23,7 +23,7 @@ export default function DayListItem(props) {
   dayClass = dayClass.replace(/\s/g, '');
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} key={props.id} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
